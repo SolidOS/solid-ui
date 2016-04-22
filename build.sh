948b874bd93e7bf5160e6e224821b888f07d15f3
Tim Berners-Lee <timbl+git@w3.org>
2016-04-21 23:53:24 -0400

    Move files from panes/common/ and convert widget.js and acl-control.js to standard

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Build the dist file based on individual modules
+# TODO: minify dist file
+
+npm run build
---
+++ index.js
@@ -0,0 +1,52 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015-2016 Solid
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+If you would like to know more about the solid Solid project, please see
+https://github.com/solid/solid
+*/
+'use strict'
+/**
+ * Provides a Solid client helper object (which exposes various static modules).
+ * @module solidUi.js
+ * @main solidUi.js
+ */
+
+/**
+ * @class SolidUi
+ * @static
+ */
+var SolidUi = {
+  acl: require('./lib/acl'),
+  aclControl: require('./lib/acl-control'),
+  icons: require('./lib/icons'),
+  matrix: require('./lib/matrix'),
+  messageArea: require('./lib/messageArea'),
+  pad: require('./lib/pad'),
+  signin: require('./lib/signin'),
+  table: require('./lib/table'),
+  utils: require('./lib/utils')
+  widgets: require('./lib/widgets')
+}
+
+module.exports = SolidUi
