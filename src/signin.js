d6b3027cbf4578d03c30f4f2360bd3d6b0fa08d3
Ruben Verborgh <ruben@verborgh.org>
2018-09-30 17:50:48 +0200

    Use default popup URI.

--- src/signin.js
+++ src/signin.js
@@ -736,7 +736,7 @@ function signInOrSignUpBox (dom, setUserCallback) {
   signInPopUpButton.addEventListener('click', () => {
     var offline = offlineTestID()
     if (offline) return setUserCallback(offline.uri)
-    return solidAuthClient.popupLogin({ popupUri: $SOLID_GLOBAL_config.popupUri })
+    return solidAuthClient.popupLogin()
       .then(session => {
         let webIdURI = session.webId
         // setUserCallback(webIdURI)
