6d5017380ab9d03d8b9ab6e321fd86d2ee69a283
Tim Berners-Lee <timbl+git@w3.org>
2018-10-17 17:23:25 +0100

    fix datauri

--- src/toDataURI.sh
+++ src/toDataURI.sh
@@ -2,5 +2,5 @@
 # from https://unix.stackexchange.com/questions/247843/how-to-generate-a-data-uri-from-an-image-file
 mimetype=$(file -bN --mime-type "$1")
 content=$(base64 < "$1")
-echo "module.export = ('data:$mimetype;base64,$content'"
+echo "module.exports =  'data:$mimetype;base64,$content'"
 # ends
