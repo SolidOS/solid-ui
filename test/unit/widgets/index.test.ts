f46d6b718933a57ee0bf8eaaab3d4de3b22e6aa1
Michiel de Jong <michiel@unhosted.org>
2020-02-03 14:24:00 +0100

    export existence test for widgets/index

---
+++ test/unit/widgets/index.test.ts
@@ -0,0 +1,44 @@
+import * as DragAndDrop from '../../../src/widgets/dragAndDrop'
+import * as Error from '../../../src/widgets/error'
+import * as Buttons from '../../../src/widgets/buttons'
+import * as Forms from '../../../src/widgets/forms'
+import * as PeoplePicker from '../../../src/widgets/peoplePicker'
+import * as Index from '../../../src/widgets/index'
+
+describe('index', () => {
+  it('contains all exports from DragAndDrop', () => {
+    for (let k in DragAndDrop) {
+      if (k !== 'default') {
+        expect(Index[k]).toEqual(DragAndDrop[k])
+      }
+    }
+  })
+  it('contains all exports from Error', () => {
+    for (let k in Error) {
+      if (k !== 'default') {
+        expect(Index[k]).toEqual(Error[k])
+      }
+    }
+  })
+  it('contains all exports from Buttons', () => {
+    for (let k in Buttons) {
+      if (k !== 'default') {
+        expect(Index[k]).toEqual(Buttons[k])
+      }
+    }
+  })
+  it('contains all exports from Forms', () => {
+    for (let k in Forms) {
+      if (k !== 'default') {
+        expect(Index[k]).toEqual(Forms[k])
+      }
+    }
+  })
+  it('contains all exports from PeoplePicker', () => {
+    for (let k in PeoplePicker) {
+      if (k !== 'default') {
+        expect(Index[k]).toEqual(PeoplePicker[k])
+      }
+    }
+  })
+})
