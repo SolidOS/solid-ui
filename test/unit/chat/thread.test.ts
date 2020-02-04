9c7826842586d1662c692c7a26ef03967c958f5a
Michiel de Jong <michiel@unhosted.org>
2020-02-04 11:36:11 +0100

    thread runs

--- test/unit/chat/thread.test.ts
+++ test/unit/chat/thread.test.ts
@@ -1,7 +1,46 @@
-import { default as Thread } from '../../../src/chat/thread'
+import { default as thread } from '../../../src/chat/thread'
   
 describe('Thread', () => {
   it('exists', () => {
-    expect(Thread).toBeInstanceOf(Function)
+    expect(thread).toBeInstanceOf(Function)
+  })
+
+  it('runs', () => {
+    const dom = {
+      createElement: () => {
+        return {
+          appendChild: () => {},
+          setAttribute: () => {},
+        }
+      }
+    }
+    const kb = {
+      query: () => {
+        return {}
+      }
+    }
+    const subject = ''
+    const messageStore = {
+      doc: () => {
+        return ''
+      }
+    }
+    const options = {}
+
+    // eslint-ignore-next-line: no-unused-vars
+    ;(window as any).$rdf = {
+      Namespace: () => {
+        return () => ''
+      },
+      Query: function () {
+        this.vars = []
+        this.pat = {
+          add: () => {}
+        }
+      },
+      variable: () => {}
+    }
+    ;(window as any).alert = () => {}
+    expect(thread(dom, kb, subject, messageStore, options)).toBeInstanceOf(Object)
   })
 })
