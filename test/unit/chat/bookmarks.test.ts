4fc1668074b6e44979d8db9fad8de5092a1107bd
Michiel de Jong <michiel@unhosted.org>
2020-02-04 11:58:49 +0100

    bookmarks runs tests

--- test/unit/chat/bookmarks.test.ts
+++ test/unit/chat/bookmarks.test.ts
@@ -1,8 +1,24 @@
+import * as rdf from 'rdflib'
 import { findBookmarkDocument, toggleBookmark, renderBookmarksButton } from '../../../src/chat/bookmarks'
 
 describe('findBookmarkDocument', () => {
   it('exists', () => {
     expect(findBookmarkDocument).toBeInstanceOf(Function)
+  })
+  it('runs', async () => {
+    const context = {
+      index: {}
+    }
+    window.alert = () => {}
+
+    const result = await findBookmarkDocument(context);
+    expect(result).toEqual({
+      containers: [],
+      index: {
+        public: [],
+      },
+      instances: [],
+    })
   })
 })
 
@@ -10,10 +26,32 @@ describe('toggleBookmark', () => {
   it('exists', () => {
     expect(toggleBookmark).toBeInstanceOf(Function)
   })
+  it.skip('runs', async () => {
+    const userContext = {
+      me: new rdf.NamedNode('http://example.com'),
+      bookmarkDocument: new rdf.NamedNode('http://example.com')
+    }
+    const target = {}
+    const bookmarkButton = {}
+    try {
+      const result = await toggleBookmark(userContext, target, bookmarkButton)
+    } catch(e) {
+      expect(e.message).toEqual('Cannot read property \'origin\' of undefined')
+    } 
+  })
 })
 
 describe('renderBookmarksButton', () => {
   it('exists', () => {
     expect(renderBookmarksButton).toBeInstanceOf(Function)
+  })
+  it.skip('runs', async () => {
+    const userContext = {
+      me: new rdf.NamedNode('http://example.com'),
+      bookmarkDocument: new rdf.NamedNode('http://example.com')
+    }
+    const target = {}
+    const result = await renderBookmarksButton(userContext, target)
+    expect(typeof result).toEqual(Object) 
   })
 })
