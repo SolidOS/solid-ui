27cc441306bb2b6364640def0e10d54d79e4e2d7
Michiel de Jong <michiel@unhosted.org>
2020-02-04 12:37:39 +0100

    jss/index runs

--- test/unit/jss/index.test.ts
+++ test/unit/jss/index.test.ts
@@ -4,11 +4,20 @@ describe('getClasses', () => {
   it('exists', () => {
     expect(getClasses).toBeInstanceOf(Function)
   })
+  it('runs', () => {
+    const insertionPoint = {} as HTMLElement
+    const styles = {}
+    expect(getClasses(insertionPoint, styles)).toBeInstanceOf(Object)
+  })
 })
 
 describe('getStylesheet', () => {
   it('exists', () => {
     expect(getStylesheet).toBeInstanceOf(Function)
+  })
+  it('runs', () => {
+    const insertionPoint = {} as HTMLElement
+    expect(getStylesheet(insertionPoint)).toBeInstanceOf(Object)
   })
 })
 
