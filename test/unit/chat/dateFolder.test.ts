5ca05753aedcfaa477c9d8f188f478d5700e9424
Michiel de Jong <michiel@unhosted.org>
2020-02-04 12:11:59 +0100

    I think we will need to mock the fetcher for these ones

--- test/unit/chat/dateFolder.test.ts
+++ test/unit/chat/dateFolder.test.ts
@@ -10,22 +10,62 @@ describe('DateFolder#leafDocumentFromDate', () => {
   it('exists', () => {
     expect(new DateFolder({ dir: () => {} }).leafDocumentFromDate).toBeInstanceOf(Function)
   })
+  it.skip('runs', () => {
+    const dateFolder = new DateFolder({ dir: () => {
+      uri: ''
+    } });
+    const result = dateFolder.leafDocumentFromDate({
+      toISOString: () => ''
+    })
+    expect(result).toEqual({})
+  })
 })
 
 describe('DateFolder#dateFromLeafDocument', () => {
   it('exists', () => {
     expect(new DateFolder({ dir: () => {} }).dateFromLeafDocument).toBeInstanceOf(Function)
   })
+  it.skip('runs', () => {
+    const dateFolder = new DateFolder({ dir: () => {
+      uri: ''
+    } });
+    const result = dateFolder.dateFromLeafDocument({
+      toISOString: () => ''
+    })
+    expect(result).toEqual({})
+  })
+
 })
 
 describe('DateFolder#loadPrevious', () => {
   it('exists', () => {
     expect(new DateFolder({ dir: () => {} }).loadPrevious).toBeInstanceOf(Function)
   })
+  it.skip('runs', async () => {
+    const dateFolder = new DateFolder({ dir: () => {
+      uri: ''
+    } });
+    const result = await dateFolder.loadPrevious({
+      toISOString: ''
+    })
+    expect(result).toEqual({})
+  })
+
 })
 
 describe('DateFolder#firstLeaf', () => {
   it('exists', () => {
     expect(new DateFolder({ dir: () => {} }).firstLeaf).toBeInstanceOf(Function)
+  })
+  it.skip('runs', async () => {
+    const dateFolder = new DateFolder({ dir: () => {
+      uri: ''
+    } });
+    ;(window as any).$rdf = {
+      graph: () => {},
+      Fetcher: function () {}
+    }
+    const result = await dateFolder.firstLeaf()
+    expect(result).toEqual({})
   })
 })
