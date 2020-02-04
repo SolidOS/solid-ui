92b4f3af37705e5d35a6daa9d6389a0da23fedc8
Michiel de Jong <michiel@unhosted.org>
2020-02-04 12:33:27 +0100

    chat/messageTools runs

--- test/unit/chat/messageTools.test.ts
+++ test/unit/chat/messageTools.test.ts
@@ -8,11 +8,19 @@ describe('sentimentStrip', () => {
   it('exists', () => {
     expect(sentimentStrip).toBeInstanceOf(Function)
   })
+  it('runs', () => {
+    expect(sentimentStrip()).toBeInstanceOf(Text)
+  })
 })
 
 describe('sentimentStripLinked', () => {
   it('exists', () => {
     expect(sentimentStripLinked).toBeInstanceOf(Function)
+  })
+  it('runs', () => {
+    const target = {}
+    const doc = {}
+    expect(sentimentStripLinked(target, doc)).toBeInstanceOf(HTMLSpanElement)
   })
 })
 
@@ -20,4 +28,8 @@ describe('messageToolbar', () => {
   it('exists', () => {
     expect(messageToolbar).toBeInstanceOf(Function)
   })
+  it('runs', () => {
+    expect(messageToolbar()).toBeInstanceOf(HTMLDivElement)
+  })
+
 })
