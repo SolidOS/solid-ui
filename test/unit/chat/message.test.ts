b5c1d37a2f553e539a9b2dcd1b6766a876b5eb81
Michiel de Jong <michiel@unhosted.org>
2020-02-04 12:22:27 +0100

    chat/message runs first two

--- test/unit/chat/message.test.ts
+++ test/unit/chat/message.test.ts
@@ -9,12 +9,32 @@ describe('elementForImageURI', () => {
   it('exists', () => {
     expect(elementForImageURI).toBeInstanceOf(Function)
   })
+  it('runs', () => {
+    const imageUri = ''
+    const options = {
+      inlineImageHeightEms: 10
+    }
+    ;(window as any).$rdf = {
+      sym: () => {}
+    }
+    expect(elementForImageURI(imageUri, options)).toBeInstanceOf(HTMLAnchorElement)
+  })
 })
 
 describe('creatorAndDate', () => {
   it('exists', () => {
     expect(creatorAndDate).toBeInstanceOf(Function)
   })
+  it('runs', () => {
+    const td1 = {
+      appendChild: () => {}
+    }
+    const creator = {}
+    const date = {}
+    const message = ''
+    expect(creatorAndDate(td1, creator, date, message)).toEqual(undefined)
+  })
+
 })
 
 describe('creatorAndDateHorizontal', () => {
