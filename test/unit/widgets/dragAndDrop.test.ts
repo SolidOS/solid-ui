25ca7d6e963908548d8b2dada2f59c030cb65f42
Michiel de Jong <michiel@unhosted.org>
2020-02-06 14:12:41 +0100

    widgets/dragAndDrop

--- test/unit/widgets/dragAndDrop.test.ts
+++ test/unit/widgets/dragAndDrop.test.ts
@@ -2,6 +2,7 @@ jest.mock('rdflib')
 import * as RdfLib from 'rdflib'
 jest.mock('solid-auth-client')
 import * as SolidAuthClient from 'solid-auth-client'
+import { element } from '../../helpers/dom'
 
 import { makeDropTarget, makeDraggable, uploadFiles } from '../../../src/widgets/dragAndDrop'
 
@@ -9,17 +10,33 @@ describe('makeDropTarget', () => {
   it('exists', () => {
     expect(makeDropTarget).toBeInstanceOf(Function)
   })
+  it('runs', () => {
+    const handler = () => {}
+    expect(makeDropTarget(element, handler, handler)).toEqual(undefined)
+  })
 })
 
 describe('makeDraggable', () => {
   it('exists', () => {
     expect(makeDraggable).toBeInstanceOf(Function)
   })
+  it('runs', () => {
+    const tr = element
+    const obj = {}
+    expect(makeDraggable(tr, obj)).toEqual(undefined)
+  })
 })
 
 
-describe('updloadFiles', () => {
+describe('uploadFiles', () => {
   it('exists', () => {
     expect(uploadFiles).toBeInstanceOf(Function)
+  })
+  it('runs', () => {
+    const files = []
+    const fileBase = ''
+    const imageBase = ''
+    const successHandler = () => {}
+    expect(uploadFiles(RdfLib.fetcher, files, fileBase, imageBase, successHandler)).toEqual(undefined)
   })
 })
