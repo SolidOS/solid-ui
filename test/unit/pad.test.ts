046959d2085693fee3b54e624140ca937409ac39
sstratsianis@gmail.com <sstratsianis@gmail.com>
2020-02-10 09:34:09 +1100

    Added exists tests to pad.test.ts

--- test/unit/pad.test.ts
+++ test/unit/pad.test.ts
@@ -1,3 +1,4 @@
+import { dom } from '../helpers/dom'
 const pad = require('../../src/pad')
 
 describe('lightColorHash', () => {
@@ -6,5 +7,36 @@ describe('lightColorHash', () => {
   })
   it('runs', () => {
     expect(pad.lightColorHash(null)).toBe('#ffffff')
+  })
+})
+describe('renderPartipants', () => {
+  it('exists', () => {
+    expect(pad.renderPartipants).toBeInstanceOf(Function)
+  })
+  // TODO: need to fix up the arguments
+  it('runs', () => {
+    const table = null
+    const padDoc = null
+    const subject = null
+    const me = null
+    const options = {}
+    expect(pad.renderPartipants(dom, table, padDoc, subject, me, options)).toBe(
+      null
+    )
+  })
+})
+describe('participationObject', () => {
+  it('exists', () => {
+    expect(pad.participationObject).toBeInstanceOf(Function)
+  })
+})
+describe('manageParticipation', () => {
+  it('exists', () => {
+    expect(pad.manageParticipation).toBeInstanceOf(Function)
+  })
+})
+describe('notepad', () => {
+  it('exists', () => {
+    expect(pad.notepad).toBeInstanceOf(Function)
   })
 })
