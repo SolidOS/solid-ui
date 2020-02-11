b38c04c12d75d5299c747d8325a2e068c4c398bb
Michiel de Jong <michiel@unhosted.org>
2020-02-11 14:27:23 +0100

    matrix.test.ts

--- test/unit/matrix.test.ts
+++ test/unit/matrix.test.ts
@@ -1,9 +1,24 @@
 import { matrixForQuery } from '../../src/matrix'
+import { JSDOM } from 'jsdom'
+
 jest.mock('rdflib')
 jest.mock('solid-auth-client')
+const window = new JSDOM('<!DOCTYPE html><p>Hello world</p>').window
+const dom = window.document
 
 describe('matrixForQuery', () => {
   it('exists', () => {
     expect(matrixForQuery).toBeInstanceOf(Function)
+  })
+  it('runs', () => {
+    expect(matrixForQuery(
+      dom,
+      '',
+      '',
+      '',
+      '',
+      {},
+      () => {}
+    )).toBeTruthy()
   })
 })
