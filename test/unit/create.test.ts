5994988fc724e0c54ef0c5752c359056511b75a7
Michiel de Jong <michiel@unhosted.org>
2020-02-11 13:40:46 +0100

    create.test.ts

--- test/unit/create.test.ts
+++ test/unit/create.test.ts
@@ -1,9 +1,16 @@
+import { JSDOM } from 'jsdom'
 import { newThingUI } from '../../src/create'
+
 jest.mock('rdflib')
 jest.mock('solid-auth-client')
+const dom = new JSDOM('<!DOCTYPE html><p>Hello world</p>').window.document
+const div = dom.createElement('div')
 
 describe('newThingUI', () => {
   it('exists', () => {
     expect(newThingUI).toBeInstanceOf(Function)
+  })
+  it('runs', () => {
+    expect(newThingUI({ dom, div }, { dom }, {})).toEqual(undefined)
   })
 })
