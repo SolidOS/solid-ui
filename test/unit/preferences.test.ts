f3cde0ef01668fdc9029430680d962419d01439e
Michiel de Jong <michiel@unhosted.org>
2020-02-11 15:00:56 +0100

    preferences

--- test/unit/preferences.test.ts
+++ test/unit/preferences.test.ts
@@ -1,6 +1,11 @@
+import { JSDOM } from 'jsdom'
+import * as RdfLib from 'rdflib'
 import Preferences from '../../src/preferences'
+
 jest.mock('rdflib')
 jest.mock('solid-auth-client')
+const window = new JSDOM('<!DOCTYPE html><p>Hello world</p>').window
+const dom = window.document
 
 describe('Preferences', () => {
   it('exists', () => {
@@ -8,9 +13,18 @@ describe('Preferences', () => {
   })
 })
 
+describe('Preferences.value', () => {
+  it('exists', () => {
+    expect(Preferences.value).toEqual([])
+  })
+})
+
 describe('Preferences.get', () => {
   it('exists', () => {
     expect(Preferences.get).toBeInstanceOf(Function)
+  })
+  it('runs', () => {
+    expect(Preferences.get(10)).toEqual(undefined)
   })
 })
 
@@ -18,11 +32,23 @@ describe('Preferences.set', () => {
   it('exists', () => {
     expect(Preferences.set).toBeInstanceOf(Function)
   })
+  it('runs', () => {
+    expect(Preferences.set(10, 'a')).toEqual(undefined)
+  })
 })
 
 describe('Preferences.renderPreferencesForm', () => {
   it('exists', () => {
     expect(Preferences.renderPreferencesForm).toBeInstanceOf(Function)
+  })
+  it('runs', () => {
+    const subject = RdfLib.sym('')
+    const klass = {}
+    const preferencesForm = {}
+    const context = { dom }
+    expect(Preferences.renderPreferencesForm(
+      subject, klass, preferencesForm, context
+    )).toBeTruthy()
   })
 })
 
@@ -30,10 +56,16 @@ describe('Preferences.recordSharedPreferences', () => {
   it('exists', () => {
     expect(Preferences.recordSharedPreferences).toBeInstanceOf(Function)
   })
+  it('runs', () => {
+    expect(Preferences.recordSharedPreferences()).toBeTruthy()
+  })
 })
 
 describe('Preferences.getPreferencesForClass', () => {
   it('exists', () => {
     expect(Preferences.getPreferencesForClass).toBeInstanceOf(Function)
+  })
+  it('runs', () => {
+    expect(Preferences.getPreferencesForClass()).toBeTruthy()
   })
 })
