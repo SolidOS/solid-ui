963b02d546d1487fbab77b867e97fbbb3bc05892
Michiel de Jong <michiel@unhosted.org>
2020-02-13 11:05:39 +0100

    (Sharon:) Add tests and prettier

---
+++ test/unit/table.test.ts
@@ -0,0 +1,16 @@
+import { JSDOM } from 'jsdom'
+import renderTableViewPane from '../../src/table'
+
+const window = new JSDOM('<!DOCTYPE html><p>Hello world</p>').window
+const dom = window.document
+
+describe('renderTableViewPane', () => {
+  it('exists', () => {
+    expect(renderTableViewPane).toBeInstanceOf(Function)
+  })
+  /*
+  it('runs', () => {
+    const options = { sourceDocument: null, tableClass: null, query: null }
+    expect(renderTableViewPane(document, options)).toEqual(undefined)
+  }) */
+})
