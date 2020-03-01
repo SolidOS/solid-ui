6a253226585ebf6e269a3672880d9de239cc310d
Arne Hassel <arne.hassel@gmail.com>
2020-03-01 12:35:22 +0100

    Migrated tabs.js to TypeScript

--- src/tabs.ts
+++ src/tabs.ts
@@ -7,19 +7,9 @@
 // written 2016-05-27
 // See https://github.com/solid/solid-ui/issues/183 for styles
 
-var tabs = {}
-module.exports = tabs
-
-var UI = {
-  icons: require('./iconBase'),
-  log: require('./log'),
-  ns: require('./ns'),
-  store: require('./store'),
-  tabs: tabs,
-  widgets: require('./widgets')
-}
-
-const utils = require('./utils')
+import store from './store'
+import { cancelButton } from './widgets'
+import { label } from './utils'
 
 // options.subject
 // options.orientation 0 top, 1 left, 2 bottom, 3 right
@@ -29,45 +19,42 @@ const utils = require('./utils')
 // options.onClose            if given, will present a cancelButton next to tabs that calls this optional method
 //
 
-UI.tabs.tabWidget = function (options) {
-  var kb = UI.store
-  var subject = options.subject
-  var dom = options.dom
-  var orientation = parseInt(options.orientation || '0')
-  var backgroundColor = options.backgroundColor || '#ddddcc'
-  var color
-  var flipped = orientation & 2
-  var vertical = orientation & 1
+export function tabWidget (options) {
+  const kb = store
+  const subject = options.subject
+  const dom = options.dom
+  const orientation = parseInt(options.orientation || '0')
+  const backgroundColor = options.backgroundColor || '#ddddcc'
+  let color
+  const flipped = orientation & 2
+  const vertical = orientation & 1
+  const onClose = options.onClose
 
-  var mainElement, navElement
-  var tabContainer, tabElement
-  var onClose = options.onClose
-
-  var isLight = function (x) {
-    var total = 0
-    for (var i = 0; i < 3; i++) {
+  const isLight = function (x) {
+    let total = 0
+    for (let i = 0; i < 3; i++) {
       total += parseInt(x.slice(i * 2 + 1, i * 2 + 3), 16)
     }
     return total > 128 * 3
   }
-  var colorBlend = function (a, b, mix) {
-    var ca, cb, res
-    var str = '#'
+  const colorBlend = function (a, b, mix) {
+    let ca, cb, res
+    let str = '#'
     const hex = '0123456789abcdef'
-    for (var i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
       ca = parseInt(a.slice(i * 2 + 1, i * 2 + 3), 16)
       cb = parseInt(b.slice(i * 2 + 1, i * 2 + 3), 16)
       res = ca * (1.0 - mix) + cb * mix // @@@ rounding
-      var res2 = parseInt(('' + res).split('.')[0]) // @@ ugh
-      var h = parseInt(('' + res2 / 16).split('.')[0]) // @@ ugh
-      var l = parseInt(('' + (res2 % 16)).split('.')[0]) // @@ ugh
+      const res2 = parseInt(('' + res).split('.')[0]) // @@ ugh
+      const h = parseInt(('' + res2 / 16).split('.')[0]) // @@ ugh
+      const l = parseInt(('' + (res2 % 16)).split('.')[0]) // @@ ugh
       str += hex[h] + hex[l]
     }
     // console.log('Blending colors ' + a + ' with ' + mix + ' of ' + b + ' to give ' + str)
     return str
   }
 
-  var selectedColor
+  let selectedColor
   if (isLight(backgroundColor)) {
     selectedColor = colorBlend(backgroundColor, '#ffffff', 0.3)
     color = '#000000'
@@ -75,69 +62,68 @@ UI.tabs.tabWidget = function (options) {
     selectedColor = colorBlend(backgroundColor, '#000000', 0.3)
     color = '#ffffff'
   }
-  var bodyMainStyle = `flex: 2; width: auto; height: 100%; border: 0.1em; border-style: solid; border-color: ${selectedColor}; padding: 1em;`
+  const bodyMainStyle = `flex: 2; width: auto; height: 100%; border: 0.1em; border-style: solid; border-color: ${selectedColor}; padding: 1em;`
 
   /*
     'resize: both; overflow: scroll; margin:0; border: 0.1em; border-style: solid; border-color: ' +
     selectedColor +
     '; padding: 1em;  min-width: 30em; min-height: 450px; width:100%;'
-*/
+  */
   const rootElement = dom.createElement('div') // 20200117a
 
-  rootElement.style = 'display: flex; height: 100%; width: 100%; flex-direction: ' +
-      (vertical ? 'row' : 'column') + (flipped ? '-reverse;' : ';')
+  rootElement.setAttribute('style', 'display: flex; height: 100%; width: 100%; flex-direction: ' +
+    (vertical ? 'row' : 'column') + (flipped ? '-reverse;' : ';'))
 
-  navElement = rootElement.appendChild(dom.createElement('nav'))
-  navElement.style = 'margin: 0;'
+  const navElement = rootElement.appendChild(dom.createElement('nav'))
+  navElement.setAttribute('style', 'margin: 0;')
 
-  mainElement = rootElement.appendChild(dom.createElement('main'))
+  const mainElement = rootElement.appendChild(dom.createElement('main'))
 
   mainElement.setAttribute('style', 'margin: 0; width:100%; height: 100%;') // override tabbedtab.css
-  tabContainer = navElement.appendChild(dom.createElement('ul'))
-  tabContainer.style = 'list-style-type: none;' + // No bullet please ...
-  'display: flex; height: 100%; width: 100%; flex-direction: ' +
-     (vertical ? 'column' : 'row') // + (flipped ? '-reverse;' : ';')
-     // Never flip the direction of readuing the tabs, assuming people read left to right and top to bottom
+  const tabContainer = navElement.appendChild(dom.createElement('ul'))
+  tabContainer.setAttribute('style', 'list-style-type: none;' + // No bullet please ...
+    'display: flex; height: 100%; width: 100%; flex-direction: ' +
+    (vertical ? 'column' : 'row')) // + (flipped ? '-reverse;' : ';')
+  // Never flip the direction of readuing the tabs, assuming people read left to right and top to bottom
 
-  tabElement = 'li'
+  const tabElement = 'li'
 
-  var bodyContainer = mainElement // .appendChild(dom.createElement('table'))
+  const bodyContainer = mainElement // .appendChild(dom.createElement('table'))
   rootElement.tabContainer = tabContainer // ussed by caller
   rootElement.bodyContainer = bodyContainer
 
-  var getItems = function () {
+  const getItems = function () {
     if (options.items) return options.items
     if (options.ordered !== false) {
       // default to true
-      var list = kb.the(subject, options.predicate)
+      const list = kb.the(subject, options.predicate)
       return list.elements
     } else {
       return kb.each(subject, options.predicate)
     }
   }
 
-  var corners = ['2em', '2em', '0', '0'] // top left, TR, BR, BL
+  let corners = ['2em', '2em', '0', '0'] // top left, TR, BR, BL
   corners = corners.concat(corners).slice(orientation, orientation + 4)
-  corners = 'border-radius: ' + corners.join(' ') + ';'
+  const cornersStyle = 'border-radius: ' + corners.join(' ') + ';'
 
-  var margins = ['0.3em', '0.3em', '0', '0.3em'] // top, right, bottom, left
+  let margins = ['0.3em', '0.3em', '0', '0.3em'] // top, right, bottom, left
   margins = margins.concat(margins).slice(orientation, orientation + 4)
-  margins = 'margin: ' + margins.join(' ') + ';'
+  const marginsStyle = 'margin: ' + margins.join(' ') + ';'
 
-  var tabStyle = corners + 'padding: 0.7em; max-width: 20em;' //  border: 0.05em 0 0.5em 0.05em; border-color: grey;
+  let tabStyle = cornersStyle + 'padding: 0.7em; max-width: 20em;' //  border: 0.05em 0 0.5em 0.05em; border-color: grey;
   tabStyle += 'color: ' + color + ';'
-  var unselectedStyle =
+  const unselectedStyle =
     tabStyle +
     'opacity: 50%; margin: 0.3em; background-color: ' +
     backgroundColor +
     ';' // @@ rotate border
-  var selectedStyle =
-    tabStyle + margins + ' background-color: ' + selectedColor + ';'
-  var shownStyle = 'height: 100%; width: 100%;'
-  var hiddenStyle = shownStyle + 'display: none;'
+  const selectedStyle = tabStyle + marginsStyle + ' background-color: ' + selectedColor + ';'
+  const shownStyle = 'height: 100%; width: 100%;'
+  const hiddenStyle = shownStyle + 'display: none;'
 
-  var resetTabStyle = function () {
-    for (var i = 0; i < tabContainer.children.length; i++) {
+  const resetTabStyle = function () {
+    for (let i = 0; i < tabContainer.children.length; i++) {
       const tab = tabContainer.children[i]
       if (tab.classList.contains('unstyled')) {
         continue
@@ -145,16 +131,16 @@ UI.tabs.tabWidget = function (options) {
       tab.firstChild.setAttribute('style', unselectedStyle)
     }
   }
-  var resetBodyStyle = function () {
-    for (var i = 0; i < bodyContainer.children.length; i++) {
+  const resetBodyStyle = function () {
+    for (let i = 0; i < bodyContainer.children.length; i++) {
       bodyContainer.children[i].setAttribute('style', hiddenStyle)
     }
   }
 
-  var makeNewSlot = function (item) {
-    var ele = dom.createElement(tabElement)
+  const makeNewSlot = function (item) {
+    const ele = dom.createElement(tabElement)
     ele.subject = item
-    var div = ele.appendChild(dom.createElement('div'))
+    const div = ele.appendChild(dom.createElement('div'))
     div.setAttribute('style', unselectedStyle)
 
     div.addEventListener('click', function (e) {
@@ -164,7 +150,7 @@ UI.tabs.tabWidget = function (options) {
       }
       div.setAttribute('style', selectedStyle)
       ele.bodyTR.setAttribute('style', shownStyle)
-      var bodyMain = ele.bodyTR.firstChild
+      let bodyMain = ele.bodyTR.firstChild
       if (!bodyMain) {
         bodyMain = ele.bodyTR.appendChild(dom.createElement('main'))
         bodyMain.setAttribute('style', bodyMainStyle)
@@ -187,19 +173,19 @@ UI.tabs.tabWidget = function (options) {
     if (options.renderTab) {
       options.renderTab(div, item)
     } else {
-      div.textContent = utils.label(item)
+      div.textContent = label(item)
     }
     return ele
   }
 
   // @@ Use common one from utils?
-  var orderedSync = function () {
-    var items = getItems()
+  const orderedSync = function () {
+    const items = getItems()
     if (!vertical) {
       // mainElement.setAttribute('colspan', items.length + (onClose ? 1 : 0))
     }
-    var slot, i, j, left, right
-    var differ = false
+    let slot, i, j, left, right
+    let differ = false
     // Find how many match at each end
     for (left = 0; left < tabContainer.children.length; left++) {
       slot = tabContainer.children[left]
@@ -222,7 +208,7 @@ UI.tabs.tabWidget = function (options) {
       }
     }
     // The elements left ... right in tabContainer.children do not match
-    var insertables = items.slice(
+    const insertables = items.slice(
       left,
       right - tabContainer.children.length + items.length + 1
     )
@@ -233,9 +219,9 @@ UI.tabs.tabWidget = function (options) {
       right -= 1
     }
     for (i = 0; i < insertables.length; i++) {
-      var newSlot = makeNewSlot(insertables[i])
-      var newBodyDiv = dom.createElement('div')
-      // var newBodyDiv = newBodyDiv.appendChild(dom.createElement('div'))
+      const newSlot = makeNewSlot(insertables[i])
+      const newBodyDiv = dom.createElement('div')
+      // const newBodyDiv = newBodyDiv.appendChild(dom.createElement('div'))
       newSlot.bodyTR = newBodyDiv
       if (left === tabContainer.children.length) {
         // None left of original on right
@@ -253,7 +239,7 @@ UI.tabs.tabWidget = function (options) {
     }
   }
 
-  var sync = function () {
+  const sync = function () {
     if (options.ordered) {
       orderedSync()
     } else {
@@ -270,9 +256,9 @@ UI.tabs.tabWidget = function (options) {
     tabContainer.children.length &&
     options.selectedTab
   ) {
-    var tab
-    var found = false
-    for (var i = 0; i < tabContainer.children.length; i++) {
+    let tab
+    let found = false
+    for (let i = 0; i < tabContainer.children.length; i++) {
       tab = tabContainer.children[i]
       if (
         tab.firstChild &&
@@ -299,11 +285,8 @@ UI.tabs.tabWidget = function (options) {
     }
     const extraTab = dom.createElement(tabElement)
     extraTab.classList.add('unstyled')
-    if (tabElement === 'td') {
-      extraTab.style.textAlign = 'right'
-    }
-    const cancelButton = UI.widgets.cancelButton(dom, onClose)
-    extraTab.appendChild(cancelButton)
+    const tabCancelButton = cancelButton(dom, onClose)
+    extraTab.appendChild(tabCancelButton)
     tabContainer.appendChild(extraTab)
     tabContainer.dataset.onCloseSet = 'true'
   }
