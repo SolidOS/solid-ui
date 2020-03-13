b42471d1443dc319c19d0f857cf0bcb0df807ffb
Michiel de Jong <michiel@unhosted.org>
2020-03-13 10:51:38 +0100

    snapshot errorMessageBlock

--- test/unit/widgets/error.test.ts
+++ test/unit/widgets/error.test.ts
@@ -5,11 +5,11 @@ import { errorMessageBlock } from '../../../src/widgets/error'
 jest.mock('solid-auth-client')
 const dom = new JSDOM('<!DOCTYPE html><p>Hello world</p>').window.document
 
-describe('button', () => {
+describe('errorMessageBlock', () => {
   it('exists', () => {
     expect(errorMessageBlock).toBeInstanceOf(Function)
   })
   it('runs', () => {
-    expect(errorMessageBlock(dom, '', undefined)).toBeTruthy()
+    expect(errorMessageBlock(dom, 'my error message', undefined)).toMatchSnapshot()
   })
 })
