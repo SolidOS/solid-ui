9fe6f4b9d0c5086e2a9f49673219e304da9c9b1f
Michiel de Jong <michiel@unhosted.org>
2020-03-16 10:55:25 +0100

    Use store.statements.slice(), fix #265

--- test/unit/helpers/clearStore.ts
+++ test/unit/helpers/clearStore.ts
@@ -1,10 +1,5 @@
 import store from '../../../src/store'
 
 export function clearStore () {
-  // FIXME: https://github.com/solid/solid-ui/issues/265
-
-  while (store.statements.length) {
-    console.log('Clearing store...')
-    store.statements.forEach(store.remove.bind(store))
-  }
+  store.statements.slice().forEach(store.remove.bind(store))
 }
