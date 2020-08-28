c32e6e0df5224ed6311eecad6b5eddb12d1620ef
Angelo Veltens <angelo.veltens@codecentric.de>
2020-08-28 08:36:26 +0200

    merge story file patterns

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
   ],
   addons: [
     "@storybook/addon-links",
