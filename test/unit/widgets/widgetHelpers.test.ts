d9cd19305c7471dec5ad5bbc473658b037bb6457
Sharon Stratsianis <sstratsianis@gmail..com>
2021-03-20 18:01:02 +1100

    fixed linting errors

--- test/unit/widgets/widgetHelpers.test.ts
+++ test/unit/widgets/widgetHelpers.test.ts
@@ -1,9 +1,9 @@
 import { silenceDebugMessages } from '../../helpers/setup'
 import { JSDOM, DOMWindow } from 'jsdom'
 import {
-    wrapDivInATR,
-    addClickListenerToElement,
-    createLinkForURI 
+  wrapDivInATR,
+  addClickListenerToElement,
+  createLinkForURI
 } from '../../../src/widgets/widgetHelpers'
 import { NamedNode } from 'rdflib'
 // @ts-ignore
@@ -21,45 +21,43 @@ let event: Event
 let clickEvent: Event
 // before Each and afterAll need to go in a high level describe
 beforeEach(() => {
-    window = new JSDOM('<!DOCTYPE html><head></head><body><p>Hello world</p></body>').window
-    dom = window.document
-    element = dom.createElement('div')
-    event = new window.Event('test')
-    clickEvent = new window.Event('click')
-    dom.dispatchEvent(event)
-  })
-  afterAll(clearStore)
+  window = new JSDOM('<!DOCTYPE html><head></head><body><p>Hello world</p></body>').window
+  dom = window.document
+  element = dom.createElement('div')
+  event = new window.Event('test')
+  clickEvent = new window.Event('click')
+  dom.dispatchEvent(event)
+})
+afterAll(clearStore)
 describe('wrapDivInATR', () => {
-    const subject = new NamedNode('https://subject#')
-    it('is NOT exposed on public API', () => {
-        expect(wrapDivInATR).toBeFalsy
-      })
-      it('returns a tr', () => {
-        wrapDivInATR(dom, element, subject)
-      })
+  const subject = new NamedNode('https://subject#')
+  it('is NOT exposed on public API', () => {
+    expect(wrapDivInATR).toBeFalsy
+  })
+  it('returns a tr', () => {
+    wrapDivInATR(dom, element, subject)
+  })
       
 });
 
 describe('addClickListenerToElement', () => {
-    // create a click event
-    it('is NOT exposed on public API', () => {
-        expect(addClickListenerToElement).toBeFalsy
-      })
-    //return a div with a click event
-   // addClickListenerToElement(element, clickEvent)
+  // create a click event
+  it('is NOT exposed on public API', () => {
+    expect(addClickListenerToElement).toBeFalsy
+  })
+  //return a div with a click event
+  // addClickListenerToElement(element, clickEvent)
 })
 
 describe('createLinkForURI', () => {
-    //create a linkIcon
-    it('is NOT exposed on public API', () => {
-        expect(createLinkForURI).toBeFalsy
-      })
+  //create a linkIcon
+  it('is NOT exposed on public API', () => {
+    expect(createLinkForURI).toBeFalsy
+  })
   //  createLinkForURI(dom, element, linkIcon) 
-    /*   {
-    const anchor = linkDiv.appendChild(linkIcon)
-    anchor.classList.add('HoverControlHide')
-    linkDiv.appendChild(dom.createElement('br'))
-  } */
+  /*   {
+  const anchor = linkDiv.appendChild(linkIcon)
+  anchor.classList.add('HoverControlHide')
+  linkDiv.appendChild(dom.createElement('br'))
+} */
 })  
-
-  
