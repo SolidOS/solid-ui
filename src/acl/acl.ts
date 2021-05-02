32fbca26cc06af3a9a9e28d2e0d633b43749228c
bourgeoa <alain.bourgeois10@gmail.com>
2021-05-02 16:41:06 +0200

    do not add acl rule without agent type

--- src/acl/acl.ts
+++ src/acl/acl.ts
@@ -231,6 +231,8 @@ export function makeACLGraphbyCombo (
 ): void {
   const ACL = ns.acl
   for (const combo in byCombo) {
+    const pairs = byCombo[combo]
+    if (!pairs.length) continue // do not add to store when no agent
     const modeURIs = combo.split('\n')
     let short = modeURIs
       .map(function (u) {
@@ -249,7 +251,6 @@ export function makeACLGraphbyCombo (
     for (let i = 0; i < modeURIs.length; i++) {
       kb.add(a, ACL('mode'), kb.sym(modeURIs[i]), aclDoc)
     }
-    const pairs = byCombo[combo]
     for (let i = 0; i < pairs.length; i++) {
       const pred = pairs[i][0]
       const ag = pairs[i][1]
