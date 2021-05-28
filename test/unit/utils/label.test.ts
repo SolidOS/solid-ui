a92be35218af43080df562af1a69d97f749481ce
Angelo Veltens <angelo.veltens@codecentric.de>
2021-05-28 19:32:50 +0200

    add tests for well-known label predicates

--- test/unit/utils/label.test.ts
+++ test/unit/utils/label.test.ts
@@ -1,12 +1,41 @@
-import { sym } from 'rdflib'
+import { lit, sym } from 'rdflib'
 import { label } from '../../../src/utils'
 import { silenceDebugMessages } from '../../helpers/setup'
+import { store } from '../../../src/logic'
+import * as ns from '../../../src/ns'
 silenceDebugMessages()
 jest.mock('solid-auth-client', () => ({
   currentSession: () => Promise.resolve(),
   trackSession: () => null
 }))
 describe('label', () => {
+  describe('uses well known label predicates', () => {
+    const thing = sym('https://resource.example/label-test#it')
+
+    beforeEach(() => {
+      store.removeDocument(thing.doc())
+    })
+
+    it.each([
+      'http://www.w3.org/ns/ui#label',
+      'http://www.w3.org/2007/ont/link#message',
+      'http://www.w3.org/2006/vcard/ns#fn',
+      'http://xmlns.com/foaf/0.1/name',
+      'http://purl.org/dc/terms/title',
+      'http://purl.org/dc/elements/1.1/title',
+      'http://purl.org/rss/1.0/title',
+      'http://www.w3.org/2000/10/swap/pim/contact#fullName',
+      'http://www.w3.org/2001/04/roadmap/org#name',
+      'http://www.w3.org/2002/12/cal/ical#summary',
+      'http://xmlns.com/foaf/0.1/nick',
+      'http://www.w3.org/2000/01/rdf-schema#label'
+    ])('renders %s as label', (property) => {
+      store.add(thing, sym(property), lit('the label'), thing.doc())
+      const result = label(thing)
+      expect(result).toEqual('the label')
+    })
+  })
+
   describe('when no label predicates are present', () => {
     it('the hostname is used when no path is present', () => {
       const result = label(sym('https://resource.example/'))
