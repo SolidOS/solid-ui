10403a69c8eea3202b2e149a1af44252d4326ff2
Timea Turdean <timea.turdean@gmail.com>
2021-09-27 17:12:45 +0200

    worked on docu for https://github.com/solid/solid-ui/issues/152

--- src/header/styleMap.ts
+++ src/header/styleMap.ts
@@ -76,6 +76,83 @@ export const styleMap = {
     height: '50px', // $icon-size - $image-margin * 2 image-margin was 5px in mashlib and icon size 60px
     width: '50px'
   },
+  'header-help-menu': {
+  },
+  'header-help-menu__link': {
+    background: 'none',
+    border: '0',
+    color: 'black',
+    cursor: 'pointer',
+    display: 'block',
+    fontFamily: 'Arial',
+    fontSize: '1em',
+    textAlign: 'left',
+    padding: '1em',
+    width: '100%',
+    '&:focus, &:hover': {
+      backgroundColor: 'linear-gradient(to right, #7C4DFF 0%, #18A9E6 50%, #01C9EA 100%)'
+    }
+  },
+  'header-help-menu__trigger': {
+    background: 'none',
+    border: '0',
+    cursor: 'pointer',
+    width: '60px', // defined in mashlib as a SASS variable $icon_size
+    height: '60px', // defined in mashlib as a SASS variable $icon_size
+    img: {
+      borderRadius: '50%',
+      height: '56px', // defined in mashlib as a SASS variable $icon_size - 4px
+      width: '56px'
+    }
+  },
+  'header-help-menu__button': {
+    background: 'none',
+    border: '0',
+    color: 'black',
+    cursor: 'pointer',
+    display: 'block',
+    fontFamily: 'Arial',
+    fontSize: '1em',
+    textAlign: 'left',
+    padding: '1em',
+    width: '100%',
+    '&:focus, &:hover': {
+      backgroundColor: 'linear-gradient(to right, #7C4DFF 0%, #18A9E6 50%, #01C9EA 100%)'
+    }
+  },
+  'header-help-menu__list': {
+    listStyle: 'none',
+    margin: '0',
+    padding: '0'
+  },
+  'header-help-menu__navigation-menu': {
+    background: 'white',
+    border: 'solid 1px #000000', // the color was defined in mashlib as a SASS variable $divider_color
+    borderRight: '0',
+    position: 'absolute',
+    right: '0',
+    top: '60px', // defined in mashlib as a SASS variable $icon_size
+    width: '200px',
+    'z-index': '1',
+    '&[aria-hidden = true]': {
+      display: 'none'
+    }
+  },
+  'header-help-menu__list-item': {
+    borderBottom: 'solid 1px #000000', // the color was defined in mashlib as a SASS variable $divider_color
+    '&:last-child': {
+      border: '0'
+    }
+  },
+  'header-help-menu__photo': {
+    borderRadius: '50%',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    margin: '5px',
+    height: '50px', // $icon-size - $image-margin * 2 image-margin was 5px in mashlib and icon size 60px
+    width: '50px'
+  },
   'header-banner': {
     boxShadow: '0px 1px 4px #000000', // the color was defined in mashlib as a SASS variable $divider_color
     display: 'flex',
@@ -95,6 +172,10 @@ export const styleMap = {
     }
   },
   'header-banner__user-menu': {
+    borderLeft: 'solid 1px #000000', // the color was defined in mashlib as a SASS variable $divider_color
+    marginLeft: 'auto'
+  },
+  'header-banner__help-menu': {
     borderLeft: 'solid 1px #000000', // the color was defined in mashlib as a SASS variable $divider_color
     marginLeft: 'auto'
   },
