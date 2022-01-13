e4feff8ac091f6b7f39cfb0fd41d779cb629ee5b
Timea Turdean <timea.turdean@gmail.com>
2022-01-13 10:42:14 +0100

    added a login text

--- test/unit/login/login.test.ts
+++ test/unit/login/login.test.ts
@@ -1,101 +1,26 @@
-import { silenceDebugMessages } from '../../helpers/setup'
-import { JSDOM } from 'jsdom'
-import {
-  defaultTestUser, // Sync
-  filterAvailablePanes, // Async
-  logIn,
-  loginStatusBox,
-  newAppInstance,
-  registrationControl,
-  registrationList,
-  selectWorkspace
-} from '../../../src/login/login'
-import { sym } from 'rdflib'
-import { AppDetails, AuthenticationContext } from 'solid-logic'
+import * as testAuthn from "../src/authn/authn";
 
-silenceDebugMessages()
-const window = new JSDOM('<!DOCTYPE html><p>Hello world</p>').window
-const dom = window.document
-
-describe('defaultTestUser', () => {
-  it('exists', () => {
-    expect(defaultTestUser).toBeInstanceOf(Function)
-  })
-  it('runs', async () => {
-    expect(await defaultTestUser()).toEqual(null)
-  })
-})
-
-describe('filterAvailablePanes', () => {
-  it('exists', () => {
-    expect(filterAvailablePanes).toBeInstanceOf(Function)
-  })
-  it('runs', async () => {
-    expect(await filterAvailablePanes([])).toEqual([])
-  })
-})
+const mockAppContext = {
+    viewingNoAuthPage: true,
+    webId: "https://timea-test.solidcommunity.net/profile/card#me"
+}
 
 describe('logIn', () => {
-  it('exists', () => {
-    expect(logIn).toBeInstanceOf(Function)
+    afterAll(() => {
+        jest.restoreAllMocks()
+    })
+    it('exists', () => {
+      expect(testAuthn.logIn).toBeInstanceOf(Function)
+    })
+    it('runs', () => {
+      expect(testAuthn.logIn({})).toBeInstanceOf(Object)
+    })
+    it('user logged in already, through appContext', async () => {
+       jest.spyOn(testAuthn, 'appContext').mockReturnValue(mockAppContext)
+       const call = await testAuthn.logIn({})
+       expect(call).toBeInstanceOf(Object)
+       console.log(call)
+       expect(testAuthn.appContext).toHaveBeenCalledTimes(1);
+        
+    })
   })
-  it('runs', () => {
-    expect(logIn({})).toBeInstanceOf(Object)
-  })
-})
-
-describe('loginStatusBox', () => {
-  it('exists', () => {
-    expect(loginStatusBox).toBeInstanceOf(Function)
-  })
-  it('runs', () => {
-    expect(loginStatusBox(dom, () => {}, {})).toBeInstanceOf(Object)
-  })
-})
-
-describe('newAppInstance', () => {
-  it('exists', () => {
-    expect(newAppInstance).toBeInstanceOf(Function)
-  })
-  it('runs', () => {
-    expect(newAppInstance(dom, {} as AppDetails, () => {})).toBeTruthy()
-  })
-})
-
-describe('registrationControl', () => {
-  it('exists', () => {
-    expect(registrationControl).toBeInstanceOf(Function)
-  })
-  it('runs', async () => {
-    expect(await registrationControl(
-      {} as AuthenticationContext,
-      sym('https://test.test#'),
-      sym('https://test.test#')
-    )).toEqual(undefined)
-  })
-})
-
-describe('registrationList', () => {
-  it('exists', () => {
-    expect(registrationList).toBeInstanceOf(Function)
-  })
-  it.skip('runs', () => {
-    expect(registrationList(
-      { dom } as AuthenticationContext,
-      {}
-    )).toEqual(undefined)
-  })
-})
-
-describe('selectWorkspace', () => {
-  it('exists', () => {
-    expect(selectWorkspace).toBeInstanceOf(Function)
-  })
-  it('runs', () => {
-    expect(selectWorkspace(
-      dom,
-      {} as AppDetails,
-      () => {}
-    )).toBeTruthy()
-  })
-})
