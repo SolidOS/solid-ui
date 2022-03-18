9bf8c5425d6f4ec43949e2c738719695fb8bb716
Timea Turdean <timea.turdean@gmail.com>
2022-03-18 18:12:22 +0100

    removed uneeded storybook addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,8 +4,7 @@ module.exports = {
   ],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-postcss"
+    "@storybook/addon-essentials"
   ],
   webpackFinal: async (config) => {
     config.externals = {
