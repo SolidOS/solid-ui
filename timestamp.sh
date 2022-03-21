93364a931468934ef515374def2e804a0788ecbf
Timea Turdean <timea.turdean@gmail.com>
2022-03-21 11:30:19 +0100

    to overcome Cannot find module './versionInfo' from 'src/index.ts'

--- timestamp.sh
+++ timestamp.sh
@@ -1,5 +1,5 @@
 # Timestamp a git/npm project in node JS
-echo "export default {"
+echo "export const versionInfo = {"
 date -u '+buildTime: "%Y-%m-%dT%H:%M:%SZ",'
 git log | grep commit | head -1 | sed -e 's/ /: "/' | sed -e 's/$/",/'
 echo npmInfo:
