0f0c08512a1f607ab4336a123c376a75d5c9bfae
Alain Bourgeois <alain.bourgeois10@gmail.com>
2023-03-03 12:13:10 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   verbose: true,
   testEnvironment: 'jsdom',
   testMatch: [
-    '//**/?(*.)+(spec|test).[tj]s?(x)'  ],
+    '<rootDir>/test/**/*.test.ts' ], //**/?(*.)+(spec|test).[tj]s?(x)'  ],
   setupFilesAfterEnv: [
     './test/helpers/setup.ts'
   ]
