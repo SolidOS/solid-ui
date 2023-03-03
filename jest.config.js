4faf24fcd16869f96dfbd372fc2557810ab25faf
Alain Bourgeois <alain.bourgeois10@gmail.com>
2023-03-03 17:33:16 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   verbose: true,
   testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+      customExportConditions: ['node']
+  },
   testMatch: [
     '<rootDir>/test/**/*.test.ts' ], //**/?(*.)+(spec|test).[tj]s?(x)'  ],
   setupFilesAfterEnv: [
