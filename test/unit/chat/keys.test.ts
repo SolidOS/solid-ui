c239a13896b77ea8de738ac554862539e3a2627b
Sharon Stratsianis <sstratsianis@gmail.com>
2023-05-03 15:22:55 +1000

    trying to mock store fetcher load function

--- test/unit/chat/keys.test.ts
+++ test/unit/chat/keys.test.ts
@@ -1,8 +1,23 @@
 import { error, log, trace, warn } from '../../src/debug'
-import { generatePrivateKey, generatePublicKey } from '../../../src/chat/keys'
-
+import { generatePrivateKey, generatePublicKey, getPublicKey } from '../../../src/chat/keys'
+import { store } from 'solid-logic'
 const PRIV_KEY = 'a11bc5d2eee6cdb3b37f5473a712cad905ccfb13fb2ccdbf1be0a1ac4fdc7d2a'
 const PUB_KEY = '023a9da707bee1302f66083c9d95673ff969b41607a66f52686fa774d64ceb87'
+
+const mockStore = jest.fn().mockImplementation(() => {
+  return {
+    fetcher: {
+      load: jest.fn()
+    }
+  }
+})
+jest.mock('solid-logic', () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      store: mockStore
+    }
+  })
+})
 
 describe('generate key pair', () => {
   // console.log('alain')
@@ -14,5 +29,12 @@ describe('generate key pair', () => {
     const publicKey = generatePublicKey(PRIV_KEY)
     expect(publicKey).toEqual(PUB_KEY)
     expect(publicKey.length).toEqual(64)
+  })
+})
+
+describe('getPublicKey', () => {
+  it('should do something', async () => {
+    const result = await getPublicKey('https://sstratsianis.solidcommunity.net/profile/card#me')
+    expect(result).toBe('')
   })
 })
