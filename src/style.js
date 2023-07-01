5ee6926378f70370e7a8f7d05e40c435aa534fff
Timea Turdean <4144203+timea-solid@users.noreply.github.com>
2023-07-01 21:47:57 +0200

    improved styles and tests

--- src/style.js
+++ src/style.js
@@ -8,17 +8,17 @@ import styleConstants from './styleConstants'
 export const style = { // styleModule
 
   checkboxStyle: 'color: black; font-size: 100%; padding-left: 0.5 em; padding-right: 0.5 em;',
-  checkboxInputStyle: 'font-size: 150%; height: 1.2em; width: 1.2em; background-color: #eef; border-radius:0.2em; margin: 0.1em',
+  checkboxInputStyle: 'font-size: 150%; height: 1.2em; width: 1.2em; background-color: #eef; border-radius:0.2em; margin: 0.1em;',
 
   fieldLabelStyle: 'color: #3B5998; text-decoration: none;',
   formSelectStyle:
-    'background-color: #eef; padding: 0.5em;  border: .05em solid #88c;  border-radius:0.2em; font-size: 100%; margin:0.4em;',
+    'background-color: #eef; padding: 0.5em;  border: .05em solid #88c; border-radius:0.2em; font-size: 100%; margin:0.4em;',
   textInputStyle:
-    'background-color: #eef; padding: 0.5em;  border: .05em solid #88c;  border-radius:0.2em; font-size: 100%; margin:0.4em;',
+    'background-color: #eef; padding: 0.5em;  border: .05em solid #88c; border-radius:0.2em; font-size: 100%; margin:0.4em;',
   textInputStyleUneditable: // Color difference only
-    'background-color: white; padding: 0.5em;  border: .05em solid white;  border-radius:0.2em; font-size: 100%; margin:0.4em;',
+    'background-color: white; padding: 0.5em;  border: .05em solid white; border-radius:0.2em; font-size: 100%; margin:0.4em;',
   buttonStyle:
-      'background-color: #fff; padding: 0.7em;  border: .01em solid white;  border-radius:0.2em; font-size: 100%; margin: 0.3em;', // 'background-color: #eef;
+      'background-color: #fff; padding: 0.7em;  border: .01em solid white; border-radius:0.2em; font-size: 100%; margin: 0.3em;', // 'background-color: #eef;
   commentStyle: 'padding: 0.7em;  border: none; font-size: 100%; white-space: pre-wrap;',
   iconStyle: 'width: 3em; height: 3em; margin: 0.1em; border-radius: 1em;',
   smallButtonStyle: 'margin: 0.2em; width: 1em; height:1em;',
@@ -53,7 +53,7 @@ export const style = { // styleModule
     `padding-left: 2em; border: 0.2em solid ${styleConstants.formBorderColor}; border-radius: 0.2em;` // @@ pink
   ],
 
-  formFieldLabelStyle: `'color: ${styleConstants.lowProfileLinkColor}; text-decoration: none;'`,
+  formFieldLabelStyle: `color: ${styleConstants.lowProfileLinkColor}; text-decoration: none;`,
   formFieldNameBoxStyle: `padding: 0.3em; vertical-align: middle; width:${styleConstants.formFieldNameBoxWidth};`,
   multilineTextInputStyle: 'font-size:100%; white-space: pre-wrap; background-color: #eef;' +
   ' border: 0.07em solid gray; padding: 1em 0.5em; margin: 1em 1em;',
@@ -64,64 +64,64 @@ export const style = { // styleModule
   linkDivStyle: 'width:2em; padding:0.5em; height: 4em;',
 
   // ACL
-  aclControlBoxContainer: 'margin: 1em',
-  aclControlBoxHeader: 'font-size: 120%; margin: 0 0 1rem',
-  aclControlBoxStatus: 'display: none; margin: 1rem 0',
-  aclControlBoxStatusRevealed: 'display: block',
-  aclGroupContent: 'maxWidth: 650',
-  accessGroupList: 'display: grid; grid-template-columns: 1fr; margin: 1em; width: 100%',
-  accessGroupListItem: 'display: grid; grid-template-columns: 100px auto 30%',
-  defaultsController: 'display: flex',
-  defaultsControllerNotice: 'color: #888; flexGrow: 1; fontSize: 80%',
-  bigButton: 'background-color: white; border: 0.1em solid #888; border-radius: 0.3em; max-width: 50%; padding-bottom: 1em; padding-top: 1em',
-  group: 'color: #888',
-  group1: 'color: green',
-  group2: 'color: #cc0',
-  group3: 'color: orange',
-  group5: 'color: red',
-  group9: 'color: blue',
-  group13: 'color: purple',
+  aclControlBoxContainer: 'margin: 1em;',
+  aclControlBoxHeader: 'font-size: 120%; margin: 0 0 1rem;',
+  aclControlBoxStatus: 'display: none; margin: 1rem 0;',
+  aclControlBoxStatusRevealed: 'display: block;',
+  aclGroupContent: 'maxWidth: 650;',
+  accessGroupList: 'display: grid; grid-template-columns: 1fr; margin: 1em; width: 100%;',
+  accessGroupListItem: 'display: grid; grid-template-columns: 100px auto 30%;',
+  defaultsController: 'display: flex;',
+  defaultsControllerNotice: 'color: #888; flexGrow: 1; fontSize: 80%;',
+  bigButton: 'background-color: white; border: 0.1em solid #888; border-radius: 0.3em; max-width: 50%; padding-bottom: 1em; padding-top: 1em;',
+  group: 'color: #888;',
+  group1: 'color: green;',
+  group2: 'color: #cc0;',
+  group3: 'color: orange;',
+  group5: 'color: red;',
+  group9: 'color: blue;',
+  group13: 'color: purple;',
 
-  trustedAppAddApplicationsTable: 'background-color: #eee',
-  trustedAppCancelButton: 'float: right',
-  trustedAppControllerI: 'border-color: orange; borderRadius: 1em; borderWidth: 0.1em',
-  temporaryStatusInit: 'background: green',
-  temporaryStatusEnd: 'background: transparent; transition: background 5s linear',
+  trustedAppAddApplicationsTable: 'background-color: #eee;',
+  trustedAppCancelButton: 'float: right;',
+  trustedAppControllerI: 'border-color: orange; border-radius: 1em; border-width: 0.1em;',
+  temporaryStatusInit: 'background: green;',
+  temporaryStatusEnd: 'background: transparent; transition: background 5s linear;',
 
   // header
-  headerUserMenuLink: 'background: none; border: 0; color: black; cursor: pointer; display: block; font-family: Arial; font-size: 1em; text-align: left; padding: 1em;  width: 100%; text-decoration: none',
-  headerUserMenuLinkHover: 'background: none; border: 0; color: black; cursor: pointer; display: block; font-family: Arial; font-size: 1em; text-align: left; padding: 1em;  width: 100%; text-decoration: none; background-image: linear-gradient(to right, #7C4DFF 0%, #18A9E6 50%, #01C9EA 100%)',
-  headerUserMenuTrigger: 'background: none; border: 0; cursor: pointer; width: 60px; height: 60px',
-  headerUserMenuTriggerImg: 'border-radius: 50%; height: 56px; width: 28px !important',
-  headerUserMenuButton: 'background: none; border: 0; color: black; cursor: pointer; display: block; font-family: Arial; font-size: 1em; text-align: left; padding: 1em; width: 100%',
-  headerUserMenuButtonHover: 'background: none; border: 0; color: black; cursor: pointer; display: block; font-family: Arial; font-size: 1em; text-align: left; padding: 1em; width: 100%; background-image: linear-gradient(to right, #7C4DFF 0%, #18A9E6 50%, #01C9EA 100%)',
-  headerUserMenuList: 'list-style: none; margin: 0; padding: 0',
-  headerUserMenuListDisplay: 'list-style: none; margin: 0; padding: 0; display:true',
-  headerUserMenuNavigationMenu: 'background: white; border: solid 1px #000000; border-right: 0; position: absolute; right: 0; top: 60px; width: 200px; z-index: 1; display: true',
-  headerUserMenuNavigationMenuNotDisplayed: 'background: white; border: solid 1px #000000; border-right: 0; position: absolute; right: 0; top: 60px; width: 200px; z-index: 1; display: none',
-  headerUserMenuListItem: 'border-bottom: solid 1px #000000',
-  headerUserMenuPhoto: 'border-radius: 50%; background-position: center; background-repeat: no-repeat; background-size: cover; height: 50px; width: 50px',
-  headerBanner: 'box-shadow: 0px 1px 4px #000000; display: flex; justify-content: space-between; padding: 0 1.5em; margin-bottom: 4px',
-  headerBannerLink: 'display: block',
-  headerBannerRightMenu: 'display: flex',
-  headerBannerLogin: 'margin-left: auto',
-  allChildrenVisible: 'display:true',
-  headerBannerUserMenu: 'border-left: solid 1px #000000; margin-left: auto',
-  headerBannerHelpMenu: 'border-left: solid 1px #000000; margin.left: auto',
-  headerBannerIcon: 'background-size: 65px 60px !important; height: 60px !important; width: 65px !important', // may just be 65px round($icon-size * 352 / 322);
+  headerUserMenuLink: 'background: none; border: 0; color: black; cursor: pointer; display: block; font-family: Arial; font-size: 1em; text-align: left; padding: 1em;  width: 100%; text-decoration: none;',
+  headerUserMenuLinkHover: 'background: none; border: 0; color: black; cursor: pointer; display: block; font-family: Arial; font-size: 1em; text-align: left; padding: 1em;  width: 100%; text-decoration: none; background-image: linear-gradient(to right, #7C4DFF 0%, #18A9E6 50%, #01C9EA 100%);',
+  headerUserMenuTrigger: 'background: none; border: 0; cursor: pointer; width: 60px; height: 60px;',
+  headerUserMenuTriggerImg: 'border-radius: 50%; height: 56px; width: 28px !important;',
+  headerUserMenuButton: 'background: none; border: 0; color: black; cursor: pointer; display: block; font-family: Arial; font-size: 1em; text-align: left; padding: 1em; width: 100%;',
+  headerUserMenuButtonHover: 'background: none; border: 0; color: black; cursor: pointer; display: block; font-family: Arial; font-size: 1em; text-align: left; padding: 1em; width: 100%; background-image: linear-gradient(to right, #7C4DFF 0%, #18A9E6 50%, #01C9EA 100%);',
+  headerUserMenuList: 'list-style: none; margin: 0; padding: 0;',
+  headerUserMenuListDisplay: 'list-style: none; margin: 0; padding: 0; display:true;',
+  headerUserMenuNavigationMenu: 'background: white; border: solid 1px #000000; border-right: 0; position: absolute; right: 0; top: 60px; width: 200px; z-index: 1; display: true;',
+  headerUserMenuNavigationMenuNotDisplayed: 'background: white; border: solid 1px #000000; border-right: 0; position: absolute; right: 0; top: 60px; width: 200px; z-index: 1; display: none;',
+  headerUserMenuListItem: 'border-bottom: solid 1px #000000;',
+  headerUserMenuPhoto: 'border-radius: 50%; background-position: center; background-repeat: no-repeat; background-size: cover; height: 50px; width: 50px;',
+  headerBanner: 'box-shadow: 0px 1px 4px #000000; display: flex; justify-content: space-between; padding: 0 1.5em; margin-bottom: 4px;',
+  headerBannerLink: 'display: block;',
+  headerBannerRightMenu: 'display: flex;',
+  headerBannerLogin: 'margin-left: auto;',
+  allChildrenVisible: 'display:true;',
+  headerBannerUserMenu: 'border-left: solid 1px #000000; margin-left: auto;',
+  headerBannerHelpMenu: 'border-left: solid 1px #000000; margin-left: auto;',
+  headerBannerIcon: 'background-size: 65px 60px !important; height: 60px !important; width: 65px !important;', // may just be 65px round($icon-size * 352 / 322);
 
   // footer
-  footer: 'border-top: solid 1px $divider-color; font-size: 0.9em; padding: 0.5em 1.5em',
+  footer: 'border-top: solid 1px $divider-color; font-size: 0.9em; padding: 0.5em 1.5em;',
 
   // buttons
-  primaryButton: 'background-color: #7c4dff; color: #ffffff; font-family: Raleway, Roboto, sans-serif; border-radius: 0.25em; border-color: #7c4dff; border: 1px solid; cursor: pointer; font-size: .8em;text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none',
-  primaryButtonHover: 'background-color: #9f7dff; color: #ffffff; font-family: Raleway, Roboto, sans-serif;border-radius: 0.25em; border-color: #7c4dff; border: 1px solid; cursor: pointer; font-size: .8em;text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none; transition: 0.25s all ease-in-out',
-  primaryButtonNoBorder: 'background-color: #ffffff; color: #7c4dff; font-family: Raleway, Roboto, sans-serif;border-radius: 0.25em; border-color: #7c4dff; border: 1px solid; cursor: pointer; font-size: .8em;text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none',
-  primaryButtonNoBorderHover: 'background-color: #7c4dff; color: #ffffff; font-family: Raleway, Roboto, sans-serif; border-radius: 0.25em; border-color: #7c4dff; border: 1px solid; cursor: pointer; font-size: .8em; text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none; transition: 0.25s all ease-in-out',
-  secondaryButton: 'background-color: #01c9ea; color: #ffffff; font-family: Raleway, Roboto, sans-serif;border-radius: 0.25em; border-color: #01c9ea; border: 1px solid; cursor: pointer; font-size: .8em;text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none',
-  secondaryButtonHover: 'background-color: #37cde6; color: #ffffff; font-family: Raleway, Roboto, sans-serif;border-radius: 0.25em; border-color: #7c4dff; border: 1px solid; cursor: pointer; font-size: .8em;text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none; transition: 0.25s all ease-in-out',
-  secondaryButtonNoBorder: 'background-color: #ffffff; color: #01c9ea; font-family: Raleway, Roboto, sans-serif; border-radius: 0.25em; border-color: #01c9ea; border: 1px solid; cursor: pointer; font-size: .8em; text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none',
-  secondaryButtonNoBorderHover: 'background-color: #01c9ea; color: #ffffff; font-family: Raleway, Roboto, sans-serif; border-radius: 0.25em; border-color: #01c9ea; border: 1px solid; cursor: pointer; font-size: .8em; text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none; transition: 0.25s all ease-in-out',
+  primaryButton: 'background-color: #7c4dff; color: #ffffff; font-family: Raleway, Roboto, sans-serif; border-radius: 0.25em; border-color: #7c4dff; border: 1px solid; cursor: pointer; font-size: .8em; text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none;',
+  primaryButtonHover: 'background-color: #9f7dff; color: #ffffff; font-family: Raleway, Roboto, sans-serif;border-radius: 0.25em; border-color: #7c4dff; border: 1px solid; cursor: pointer; font-size: .8em; text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none; transition: 0.25s all ease-in-out;',
+  primaryButtonNoBorder: 'background-color: #ffffff; color: #7c4dff; font-family: Raleway, Roboto, sans-serif;border-radius: 0.25em; border-color: #7c4dff; border: 1px solid; cursor: pointer; font-size: .8em; text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none;',
+  primaryButtonNoBorderHover: 'background-color: #7c4dff; color: #ffffff; font-family: Raleway, Roboto, sans-serif; border-radius: 0.25em; border-color: #7c4dff; border: 1px solid; cursor: pointer; font-size: .8em; text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none; transition: 0.25s all ease-in-out;',
+  secondaryButton: 'background-color: #01c9ea; color: #ffffff; font-family: Raleway, Roboto, sans-serif;border-radius: 0.25em; border-color: #01c9ea; border: 1px solid; cursor: pointer; font-size: .8em; text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none;',
+  secondaryButtonHover: 'background-color: #37cde6; color: #ffffff; font-family: Raleway, Roboto, sans-serif;border-radius: 0.25em; border-color: #7c4dff; border: 1px solid; cursor: pointer; font-size: .8em; text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none; transition: 0.25s all ease-in-out;',
+  secondaryButtonNoBorder: 'background-color: #ffffff; color: #01c9ea; font-family: Raleway, Roboto, sans-serif; border-radius: 0.25em; border-color: #01c9ea; border: 1px solid; cursor: pointer; font-size: .8em; text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none;',
+  secondaryButtonNoBorderHover: 'background-color: #01c9ea; color: #ffffff; font-family: Raleway, Roboto, sans-serif; border-radius: 0.25em; border-color: #01c9ea; border: 1px solid; cursor: pointer; font-size: .8em; text-decoration: none; padding: 0.5em 4em; transition: 0.25s all ease-in-out; outline: none; transition: 0.25s all ease-in-out;',
 
   // media
   controlStyle: `border-radius: 0.5em; margin: 0.8em; width:${styleConstants.mediaModuleCanvasWidth}; height:${styleConstants.mediaModuleCanvasHeight};`,
@@ -137,8 +137,8 @@ export const style = { // styleModule
 
   // pad
   notepadStyle: 'padding: 1em; overflow: auto; resize: horizontal; min-width: 40em;',
-  upstreamStatus: 'width:50%',
-  downstreamStatus: 'width:50%',
+  upstreamStatus: 'width: 50%;',
+  downstreamStatus: 'width: 50%;',
   baseStyle: 'font-size: 100%; font-family: monospace; width: 100%; border: none; white-space: pre-wrap;',
   headingCore: 'font-family: sans-serif; font-weight: bold;  border: none;',
   headingStyle: [
@@ -157,7 +157,7 @@ export const style = { // styleModule
   tabsRootElement: 'display: flex; height: 100%; width: 100%;',
   tabsMainElement: 'margin: 0; width:100%; height: 100%;',
   tabContainer: 'list-style-type: none; display: flex; height: 100%; width: 100%; margin: 0; padding: 0;',
-  makeNewSlot: 'background: none; border: none; font: inherit; cursor: pointer',
+  makeNewSlot: 'background: none; border: none; font: inherit; cursor: pointer;',
   ellipsis: 'position: absolute; right: 0; bottom: 0; width: 20%; background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;'
 
 }
