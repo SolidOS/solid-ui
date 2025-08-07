cb21b071dd73150690b30176cd8bd9dd654ba721
timea-solid <4144203+timea-solid@users.noreply.github.com>
2025-08-07 14:34:40 +0200

    storybook to v7.x

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,19 @@
-module.exports = {
+export default {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-actions',
-    '@storybook/addon-essentials'
+    '@storybook/addon-essentials',
+    '@storybook/addon-mdx-gfm'
   ],
-  core: {
-    builder: 'webpack5'
+
+  framework: {
+    name: '@storybook/html-webpack5',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
   }
 }
