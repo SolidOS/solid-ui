66bbb373871dfc7358579b6b0be49439f7da2639
timea-solid <4144203+timea-solid@users.noreply.github.com>
2025-08-19 16:23:29 +0200

    imporved timestamp bash

--- timestamp.sh
+++ timestamp.sh
@@ -1,7 +1,13 @@
-# Timestamp a git/npm project in node JS
-echo "export const versionInfo = {"
+#!/bin/bash
+
+echo "export default {"
 date -u '+buildTime: "%Y-%m-%dT%H:%M:%SZ",'
 git log | grep commit | head -1 | sed -e 's/ /: "/' | sed -e 's/$/",/'
-echo npmInfo:
-npm version
+echo "  npmInfo: {"
+  npm version | sed 's/\x1b\[[0-9;:]*[mG]//g' | grep -v '^{' | while read line; do
+    key=$(echo "$line" | cut -d ':' -f 1 | tr -d ' ')
+    value=$(echo "$line" | cut -d ':' -f 2- | tr -d ' ')
+    echo "    \"${key}\": \"${value}\","
+  done
+echo "  }"
 echo "};"
