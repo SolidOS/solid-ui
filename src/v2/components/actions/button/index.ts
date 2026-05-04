5ba5ac551cf6bec4adf46ab40edf0e6a14234e87
Sharon Stratsianis <sstratsianis@gmail.com>
2026-05-05 07:45:53 +1000

    button type for using in js

--- src/v2/components/actions/button/index.ts
+++ src/v2/components/actions/button/index.ts
@@ -4,6 +4,12 @@ export { Button }
 
 const BUTTON_TAG_NAME = 'solid-ui-button'
 
+declare global {
+  interface HTMLElementTagNameMap {
+    'solid-ui-button': Button
+  }
+}
+
 if (!customElements.get(BUTTON_TAG_NAME)) {
   customElements.define(BUTTON_TAG_NAME, Button)
 }
