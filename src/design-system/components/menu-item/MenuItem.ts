934844330be4772f80ec1344ac5791e20ce13a93
Noel De Martin <noel@noeldemartin.com>
2026-06-02 10:02:51 +0200

    #769 Add link menu items

--- src/design-system/components/menu-item/MenuItem.ts
+++ src/design-system/components/menu-item/MenuItem.ts
@@ -1,4 +1,4 @@
-import { customElement } from 'lit/decorators.js'
+import { customElement, property } from 'lit/decorators.js'
 import WebComponent from '../../../primitives/lib/WebComponent'
 import { html } from 'lit'
 
@@ -8,7 +8,20 @@ import styles from './MenuItem.styles.css'
 export default class MenuItem extends WebComponent {
   static styles = styles
 
+  @property({ type: String })
+  accessor href: string | undefined
+
   render () {
+    if (this.href) {
+      return html`
+        <a href="${this.href}" target="_blank" rel="noopener noreferrer">
+            <slot name="left-icon"></slot>
+            <slot></slot>
+            <slot name="right-icon"></slot>
+        </a>
+      `
+    }
+
     return html`
         <button type="button">
             <slot name="left-icon"></slot>
