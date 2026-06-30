082b9ee1eee178ea571c838996d75a9434290f3b
timea-solid <4144203+timea-solid@users.noreply.github.com>
2026-06-30 12:35:46 +0200

    added an updater to the storybook store

--- src/storybook/store/StorybookStore.ts
+++ src/storybook/store/StorybookStore.ts
@@ -3,5 +3,12 @@ import * as rdf from 'rdflib'
 import { LiveStore } from 'rdflib'
 
 export default class StorybookStore implements StoreContext {
-  public store: LiveStore = rdf.graph() as LiveStore
+  public store: LiveStore = createStore()
+}
+
+function createStore (): rdf.LiveStore {
+  const store = rdf.graph() as LiveStore
+  store.updater = new rdf.UpdateManager(store) // Add real-time live updates store.updater
+  store.features = [] // disable automatic node merging on store load
+  return store
 }
