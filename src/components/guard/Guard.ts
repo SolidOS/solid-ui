6b36abfbc30d18d9d1d6fa095551db282cb8da31
Noel De Martin <noel@noeldemartin.com>
2026-07-14 15:12:45 +0200

    Fix Guard component prefix

--- src/components/guard/Guard.ts
+++ src/components/guard/Guard.ts
@@ -3,7 +3,7 @@ import { consume } from '@lit/context'
 import { html } from 'lit'
 import { authContext, AuthContext, DEFAULT_AUTH_CONTEXT } from '@/lib/auth'
 
-@customElement('solid-guard')
+@customElement('solid-ui-guard')
 export default class Guard extends WebComponent {
   @consume({ context: authContext, subscribe: true })
   private accessor auth: AuthContext = DEFAULT_AUTH_CONTEXT
